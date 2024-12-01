d53576e8f86f3165adedf13d5667def4dc645cb8
Noah Park-Nguyen <noahparknguyen@gmail.com>
2024-12-01 17:46:44 -0500

    updated path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/repo/",
+  base: "/Wordle-Project/",
   plugins: [react()],
   server: {
     host: true,
