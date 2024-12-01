f8525715155860b1e1df73fa4e72515b1406ad91
Noah Park-Nguyen <noahparknguyen@gmail.com>
2024-12-01 18:03:38 -0500

    update repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Wordle-Project/",
+  base: "/Wordle-Clone/",
   plugins: [react()],
   server: {
     host: true,
